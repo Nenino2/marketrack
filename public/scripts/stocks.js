062e46054b2a4017edbc14be77122727595009ca
halbtale <halbtale@gmail.com>
2021-05-16 20:13:52 +0200

    Added filters

--- public/scripts/stocks.js
+++ public/scripts/stocks.js
@@ -2,26 +2,30 @@ const db = firebase.firestore();
 const stockListDom = document.getElementById('stock-list')
 const searchButtonDom = document.getElementById('search-button')
 const searchInputDom = document.getElementById('search-input')
+const minPriceInputDom = document.getElementById('min-price-input')
+const maxPriceInputDom = document.getElementById('max-price-input')
 const nextButtonDom = document.getElementById('next-button')
 
 let docLast = null;
 
 async function searchWithOptions() {
     const textValue = searchInputDom.value;
+    const minPrice = minPriceInputDom.value;
+    const maxPrice = maxPriceInputDom.value;
     const optionValue = document.querySelector('input[name="target"]:checked').value;
-    if (!textValue || !optionValue) return load()
+    if (!textValue || !optionValue) return load({minPrice, maxPrice })
 
     if (optionValue === 'name' || optionValue === 'isin') {
-        await searchByKeyValue(optionValue, textValue)
+        await searchByKeyValue(optionValue, textValue, {minPrice, maxPrice})
     } else if (optionValue === 'market') {
-        await searchByKeyValueInArray(optionValue, textValue)
+        await searchByKeyValueInArray(optionValue, textValue, {minPrice, maxPrice})
     }
 }
-searchButtonDom.addEventListener('click', () => {docLast = null;searchWithOptions})
+searchButtonDom.addEventListener('click', () => {docLast = null;searchWithOptions()})
 
 nextButtonDom.addEventListener('click', async () => {
     if (!searchInputDom.value) {
-        await load()
+        await load({minPrice: minPriceInputDom.value, maxPrice: maxPriceInputDom.value })
     } else {
         await searchWithOptions();
     }
@@ -47,13 +51,20 @@ function displayStock(doc, stock) {
     docLast = doc;
 }
 
-async function load() {
-    let stocksSnapshots;
-    if (!docLast) {
-        stocksSnapshots = await db.collection("STOCKS").limit(8).get();
+async function load(filters = {}) {
+    let stocksSnapshots = db.collection("STOCKS").orderBy('closingPrice');
+    if (docLast) {
+        stocksSnapshots = stocksSnapshots.startAfter(docLast)
     } else {
-        stocksSnapshots = await db.collection("STOCKS").startAfter(docLast).limit(8).get();
+        if (filters.minPrice) {
+            console.log('ok')
+            stocksSnapshots = stocksSnapshots.startAt(parseFloat(filters.minPrice))
+        }
+        if (filters.maxPrice) {
+            stocksSnapshots = stocksSnapshots.endAt(parseFloat(filters.maxPrice))
+        }
     }
+    stocksSnapshots = await stocksSnapshots.limit(8).get()
     clearStockList();
     stocksSnapshots.forEach((doc) => {
         const stock = doc.data()
@@ -61,13 +72,19 @@ async function load() {
     })
 }
 
-async function searchByKeyValue(key, value) {
-    let stocksSnapshots;
-    if (!docLast) {
-        stocksSnapshots = await db.collection("STOCKS").where(key, "==", value).limit(8).get();
+async function searchByKeyValue(key, value, filters = {}) {
+    let stocksSnapshots = db.collection("STOCKS").where(key, "==", value).orderBy('closingPrice');
+    if (docLast) {
+        stocksSnapshots = stocksSnapshots.startAfter(docLast)
     } else {
-        stocksSnapshots = await db.collection("STOCKS").where(key, "==", value).startAfter(docLast).limit(8).get();
+        if (filters.minPrice) {
+            stocksSnapshots = stocksSnapshots.startAt(filters.minPrice)
+        }
+        if (filters.maxPrice) {
+            stocksSnapshots = stocksSnapshots.endAt(filters.maxPrice)
+        }
     }
+    stocksSnapshots = await stocksSnapshots.limit(8).get()
     
     clearStockList();
     stocksSnapshots.forEach((doc) => {
@@ -76,13 +93,19 @@ async function searchByKeyValue(key, value) {
     })
 }
 
-async function searchByKeyValueInArray(key, value) {
-    let stocksSnapshots;
-    if (!docLast) {
-        stocksSnapshots = await db.collection("STOCKS").where(key, "array-contains", value).limit(8).get();
+async function searchByKeyValueInArray(key, value, filters) {
+    let stocksSnapshots = db.collection("STOCKS").where(key, 'array-contains', value).orderBy('closingPrice');
+    if (docLast) {
+        stocksSnapshots = stocksSnapshots.startAfter(docLast)
     } else {
-        stocksSnapshots = await db.collection("STOCKS").where(key, "array-contains", value).startAfter(docLast).limit(8).get();
+        if (filters.minPrice) {
+            stocksSnapshots = stocksSnapshots.startAt(filters.minPrice)
+        }
+        if (filters.maxPrice) {
+            stocksSnapshots = stocksSnapshots.endAt(filters.maxPrice)
+        }
     }
+    stocksSnapshots = await stocksSnapshots.limit(8).get()
     clearStockList();
     stocksSnapshots.forEach((doc) => {
         const stock = doc.data()
