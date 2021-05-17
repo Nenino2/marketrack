a6b0a857cc11910f83969c407d9e2561ad2bba24
halbtale <halbtale@gmail.com>
2021-05-17 17:26:04 +0200

    [main] Fixed

--- public/scripts/market.js
+++ public/scripts/market.js
@@ -16,19 +16,34 @@ async function load() {
                     <div class="col-header">NAME</div>
                     <div class="col-content">${market['Name']}</div>
                 </div>
-                
-                ${
-                    Object.keys(market['Hours']).map(key => {
-                        return `
-                            <div class="row">
-                                <div class="col-header">HOURS-${key.toUpperCase()}</div>
-                                <div class="col-content">${market['Hours'][key]}</div>
-                                
-                            </div>
-                            `
-                    }).join('')
-                }
-          
+                <div class="row">
+                    <div class="col-header">HOURS-MONDAY</div>
+                    <div class="col-content">${market['Hours']['Monday']}</div>
+                </div>
+                <div class="row">
+                    <div class="col-header">HOURS-TUESDAY</div>
+                    <div class="col-content">${market['Hours']['Tuesday']}</div>
+                </div>
+                <div class="row">
+                    <div class="col-header">HOURS-WEDNESDAY</div>
+                    <div class="col-content">${market['Hours']['Wednesday']}</div>
+                </div>
+                <div class="row">
+                    <div class="col-header">HOURS-THURSDAY</div>
+                    <div class="col-content">${market['Hours']['Thursday']}</div>
+                </div>
+                <div class="row">
+                    <div class="col-header">HOURS-FRIDAY</div>
+                    <div class="col-content">${market['Hours']['Friday']}</div>
+                </div>
+                <div class="row">
+                    <div class="col-header">HOURS-SATURDAY</div>
+                    <div class="col-content">${market['Hours']['Saturday']}</div>
+                </div>
+                <div class="row">
+                    <div class="col-header">HOURS-SUNDAY</div>
+                    <div class="col-content">${market['Hours']['Sunday']}</div>
+                </div>
         `)
     })
 }
